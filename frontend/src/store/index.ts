700716ea598eb2507a4a5fb48dce32612c47fe47
Harshil Madhu (Rx) <radixdt.3345@gmail.com>
2026-08-27 11:07:51 +0530

    feat(#35): register leaveTypeReducer in store

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authenticationReducer from './authenticationSlice';
 import employeeReducer from './employeeSlice';
+import leaveTypeReducer from './leaveTypeSlice';
 
 export const store = configureStore({
   reducer: {
     authentication: authenticationReducer,
     employee: employeeReducer,
+    leaveType: leaveTypeReducer,
   },
 });
 
