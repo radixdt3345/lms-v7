9590ea75678ad37793491ed525422f815e859da3
Harshil Madhu (Rx) <radixdt.3345@gmail.com>
2026-08-27 11:15:46 +0530

    feat(#89): register auditLogReducer in store

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,13 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authenticationReducer from './authenticationSlice';
 import employeeReducer from './employeeSlice';
-import leaveTypeReducer from './leaveTypeSlice';
+import { auditLogReducer } from './auditLogSlice';
 
 export const store = configureStore({
   reducer: {
     authentication: authenticationReducer,
     employee: employeeReducer,
-    leaveType: leaveTypeReducer,
+    auditLog: auditLogReducer,
   },
 });
 
